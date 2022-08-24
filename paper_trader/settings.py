5b1a5196be17198fd462c02c38bccb51c14563a1
charliubao <107311252+charliubao@users.noreply.github.com>
2022-08-24 13:09:41 -0400

    Update settings.py

--- paper_trader/settings.py
+++ paper_trader/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%1qk!2&e*rb#0!=%b%@a&9q=4)+x!cgvjc!7h63h3h*ow5ga+'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
